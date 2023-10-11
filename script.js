d801cbd0a222e366566ed5b90757a34b839fda29
Gabriel Drozdov <gabrieldrozdov@gmail.com>
2023-10-11 13:32:03 -0400

    webcam test

---
+++ script.js
@@ -0,0 +1,14 @@
+let capture;
+
+function setup() {
+  createCanvas(390, 240);
+  capture = createCapture(VIDEO);
+  capture.size(320, 240);
+  capture.hide();
+}
+
+function draw() {
+  background(255);
+  image(capture, 0, 0, 320, 240);
+  filter(INVERT);
+}
