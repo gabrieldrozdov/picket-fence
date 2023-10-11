05e2098a072882193933355d65f1c38c19144528
Gabriel Drozdov <gabrieldrozdov@gmail.com>
2023-10-11 14:01:35 -0400

    camera test2

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
 let capture;
 
 function setup() {
-  createCanvas(390, 240);
-  capture = createCapture({ video: { facingMode: "user" } });
-  capture.size(320, 240);
-  capture.hide();
+	var canvas = createCanvas(1920, 1080);
+	capture = createCapture({ video: { facingMode: "environment" } });
+	capture.size(1920, 1080);
+	capture.hide();
+	canvas.parent('parent')
 }
 
 function draw() {
-  background(255);
-  image(capture, 0, 0, 320, 240);
-  filter(INVERT);
+	background(255);
+	image(capture, 0, 0, 1920, 1080);
 }
+
+
+// window.addEventListener( 
+// 	'click', async function () { 
+
+// 		let features = { 
+// 			audio: true, 
+// 			video: { 
+// 				width: { ideal: 1800 }, 
+// 				height: { ideal: 900 } 
+// 			} 
+// 		}; 
+
+// 		let display = await navigator.mediaDevices 
+// 			.getUserMedia(features); 
+
+// 		// Returns a sequence of MediaStreamTrack objects  
+// 		// representing the video tracks in the stream 
+
+// 		let settings = display.getVideoTracks()[0] 
+// 			.getSettings(); 
+
+// 		let width = settings.width; 
+// 		let height = settings.height; 
+
+// 		console.log('Actual width of the camera video: ' 
+// 			+ width + 'px'); 
+// 		console.log('Actual height of the camera video:' 
+// 			+ height + 'px'); 
+// 	}); 
