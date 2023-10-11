f92a901f7c73bed523c386ce7690504db15bfc2c
Gabriel Drozdov <gabrieldrozdov@gmail.com>
2023-10-11 15:58:38 -0400

    test

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let capture;
 function setup() {
 	var canvas = createCanvas(1920, 1440);
 	capture = createCapture({
-		video: { facingMode: "environment" },
+		video: { facingMode: "user" },
 		audio: false,
 	});
 	// capture.size(1920, 1440);
@@ -18,18 +18,37 @@ let properties = {
 	unit: 3,
 	frames: 5,
 	speed: 1,
+	orientation: true,
+	direction: true
 }
 function draw() {
-	// Move the rectangles
-	offset += properties['speed']/10;
-	if (offset >= width) {
-		offset = 0;
-	}
 	image(capture, 0, 0, 1920, 1440);
 
-	// Draw the rectangles
-	for (let i=-width; i<width*2; i++) {
-		rect(offset+i*properties['frames']*properties['unit'], 0, properties['unit']*(properties['frames']-1), height);
+	let direction = 1;
+	if (!properties['direction']) {
+		direction = -1;
+	}
+
+	if (properties['orientation']) {
+		// Move the rectangles
+		offset += properties['speed']/10 * direction;
+		if (offset >= width) {
+			offset = 0;
+		}
+		// Draw the rectangles
+		for (let i=-width; i<width*2; i++) {
+			rect(offset+i*properties['frames']*properties['unit'], 0, properties['unit']*(properties['frames']-1), height);
+		}
+	} else {
+		// Move the rectangles
+		offset += properties['speed']/10 * direction;
+		if (offset >= height) {
+			offset = 0;
+		}
+		// Draw the rectangles
+		for (let i=-width; i<width*2; i++) {
+			rect(0, offset+i*properties['frames']*properties['unit'], width, properties['unit']*(properties['frames']-1));
+		}
 	}
 }
 
@@ -50,6 +69,17 @@ function imposeMinMax(e){
 	} else {
 		e.value = e.min;
 	}
+}
+
+// Flip orientation
+function flipOrientation() {
+	properties['orientation'] = !properties['orientation'];
+	offset = 0;
+}
+
+// Flip direction
+function flipDirection() {
+	properties['direction'] = !properties['direction'];
 }
 
 // window.addEventListener( 
